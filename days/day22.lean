94810c55ab9abdfebee7c2019c7febd740965a36
Oleg Nizhnikov <odomontois@gmail.com>
2022-12-22 22:49:39 +0100

    day22 p2

--- days/day22.lean
+++ days/day22.lean
@@ -1,20 +1,34 @@
 import Advent
 import Std
 
+open Lean (RBMap HashMap)
+
+inductive Dir | R | D | L | U
+deriving Inhabited, Lean.ToJson, Repr, BEq, Ord, Hashable
+
+inductive Turn | Straight| Linksaf| Rechtsaf
+deriving Inhabited, Lean.ToJson, Repr, BEq, Ord, Hashable
+
+structure Side where
+  x: Nat
+  y: Nat
+  dir: Dir
+deriving BEq, Ord, Hashable, Lean.ToJson, Repr
+
+abbrev Glue := Side × Side
+
 structure Map where
   n: Nat
   m: Nat
-  elements :Array (Array Char)
-  hportals :Array (Nat × Nat)
-  vportals :Array (Nat × Nat)
+  elements: Array (Array Char)
+  side: Nat
+  glues: Array Glue
   trace :Array (Array Char) := elements
-  gone: Array (Nat × Nat) := Array.empty
-  cubic: Bool := false
 deriving Inhabited, Lean.ToJson, Repr, BEq
 
-inductive Dir | R | D | L | U
-deriving Inhabited, Lean.ToJson, Repr, BEq, Ord
 
+
+open Turn
 namespace Dir
 def rechtsaf: Dir -> Dir
 | R => D
@@ -28,17 +42,31 @@ def linksaf: Dir -> Dir
 | U => L
 | R => U
 
+def turn: Turn -> Dir -> Dir
+| Turn.Straight  => id
+| Turn.Linksaf => linksaf
+| Turn.Rechtsaf=> rechtsaf 
+
 def code: Dir -> Fin 4 
 | R => 0
 | D => 1
 | L => 2
 | U => 3
 
+def back: Dir -> Dir 
+| R => L
+| L => R
+| U => D
+| D => U
+
 theorem leftRight (d: Dir): d.linksaf.rechtsaf = d := by
   cases d <;> simp [linksaf, rechtsaf] 
 
 theorem rightLeft (d: Dir): d.rechtsaf.linksaf = d := by
   cases d <;> simp [linksaf, rechtsaf]
+
+theorem backBack (d: Dir): d.back.back = d := by
+  cases d <;> simp [back]
 
 def parse: Parse Dir := Parse.choose [("R", R), ("D", D), ("U", U), ("L", L)] 
 end Dir
@@ -48,15 +76,12 @@ variable (map: Map)
 
 def get (x y: Nat): Char := map.elements[x]![y]!  
 
-def start: (Nat × Nat) := (0, (map.elements[0]!.indexOf? '.').elim 0 (· + 0) )
 
-def step (x y: Nat)(d: Dir): (Nat × Nat) := 
+def step (x y: Nat) (d: Dir): (Dir × Nat × Nat) := 
   let (x', y') := move x y d 
-  let (x', y') := 
-    if x' == map.m || y' == map.n then teleport x y d
-    else if map.get x' y' == ' ' then teleport x y d
-    else (x', y')
-  if map.elements[x']![y']! == '#' then (x, y) else (x', y')
+  let tele := if x' == map.m || y' == map.n then true else map.get x' y' == ' '
+  let (d', x', y') := if tele then teleport x y d else (d, x', y')
+  if map.elements[x']![y']! == '#' then (d, x, y) else (d',x', y')
 where 
   move 
   | x, y, R => (x, y + 1)
@@ -65,78 +90,111 @@ where
   | x, y + 1, L =>(x, y)
   | 0, y, U => (map.m, y)
   | x + 1, y, U => (x, y)
-  teleport
-  | x, _, R => (x, map.hportals[x]!.1)
-  | x, _, L => (x, map.hportals[x]!.2)
-  | _, y, D => (map.vportals[y]!.1, y)
-  | _, y, U => (map.vportals[y]!.2, y)
+  invert u := map.side - u - 1
+  leave x y: Dir -> Nat
+  | R => x
+  | L => invert x
+  | U => y
+  | D => invert y  
+  enter q : Dir -> (Nat × Nat)
+  | R => (q, invert 0)
+  | L => (invert q, 0)
+  | U => (0, q)
+  | D => (invert 0, invert q)
+  findGlue x y d : Option Side := 
+    let s := ⟨x / map.side, y / map.side, d⟩
+    (map.glues.find? (·.1 == s) ).map (·.2)
+  teleport x y d := 
+    if let some ⟨i, j, dir⟩ := findGlue x y d then
+      let q := invert <| leave (x % map.side) (y % map.side) d
+      let (dx, dy) := enter q dir
+      (dir.back, i * map.side + dx, j * map.side + dy)
+    else
+      panic! s!"can't find glue for {x} {y} {d}"
+structure Step where
+  map: Map
+  dir: Dir
+  x: Nat 
+  y: Nat
 
-def stepTrace (d: Dir): (Map × Nat × Nat) -> (Map × Nat × Nat)
-| (map, (x, y)) =>  
-  let char := match d with 
+def start: Step := {map , dir := R, x := 0, y := (map.elements[0]!.indexOf? '.').elim 0 (· + 0) }
+
+def stepTrace: Step -> Step
+| {map, dir, x, y} =>  
+  let char := match dir with 
   | D => 'V'
   | R => '>'
   | L => '<'
   | U => '^'
-  let (x', y') := map.step x y d
-  ({map with 
-    trace := map.trace.modify x (·.set! y char)
-    gone := map.gone.push (x, y)
-  }, (x', y'))
+  let map := {map with trace := map.trace.modify x (·.set! y char)}
+  let (dir, x, y) := map.step x y dir
+  {map, dir, x, y}
   
 
-def walk( xs: List (Nat × Dir)): (Map × (Nat × Nat)) := 
-  xs.foldl (fun (m, p) (c, d) => compN (stepTrace d) c (m, p)) (map, map.start)
+def walkBunch (c: Nat) (t: Turn) (s: Step): Id Step := do
+  let mut s := {s with dir := s.dir.turn t}
+  for _ in [0:c] do
+    s := stepTrace s
+  return s
 
-def make (ls: List String): Map := 
+
+def walk( xs: List (Nat × Turn)): Step := 
+  xs.foldl (fun pos (c, t) => walkBunch c t pos) map.start
+
+def make (ls: List String) (side: Nat) (glues: Array Glue): Map := 
   let elements := (ls.map (·.toList.toArray)).toArray
   let n := elements.foldl (max · ·.size) 0
   let m := elements.size
   let elements := elements.map (·.autoGrow n ' ')
-  {elements, n, m, hportals:= horPortals elements , vportals:=  verPortals elements}
-where 
-  horPortals (elems: Array (Array Char)): Array  (Nat × Nat) := elems.map <| fun arr => 
-    let x := (arr.findIdx? (· != ' ')).getD 0
-    let y := arr.size -  (arr.reverse.findIdx? (· != ' ')).getD 0 - 1
-    (x, y)
-
-  verPortals elems := elems.transpose |> horPortals
+  {elements, n, m, side, glues}
 
 def strings := map.trace.map (String.mk <| ·.toList)
 end Map
 
-def parseCommande: Parse (List (Nat × Dir)) :=  do
+def parseCommande: Parse (List (Nat × Turn)) :=  do
   let init <- Parse.nat
   let ls <- parseOne.rep
-  let tts := ls.scanl (fun (_, d) (x, tl) => (x, if tl then d.linksaf else d.rechtsaf) ) (init, R)
+  let tts := (init, Straight) :: ls
   return tts
 where
   parseOne := do
-  let turn <- Parse.choose [("L", true), ("R", false)]
+  let turn <- Parse.choose [("L", Linksaf), ("R", Rechtsaf)]
   let cnt <- Parse.nat  
   return (cnt, turn)
+
+def parseGlues: Parse (Array Glue) := do
+  let glues <- glue.repSep! ", "
+  let glues := glues.toArray
+  let glues' := glues.map (fun (l, r) => (r, l))
+  return glues ++ glues'
+where
+  side: Parse Side := open Parse in do
+    let x <- nat
+    ws
+    let y <- nat
+    ws
+    let dir <- Dir.parse
+    return {x, y, dir}
+  glue := do
+    let left <- side
+    Parse.str " <-> "
+    let right <- side
+    return (left, right)
 
 def main: IO Unit := do
   let lines <- readLines 22
   let map := lines.takeWhile (· != "")
-  let last := lines.dropWhile (· != "") |> (·[1]!)
-
-  map.forM IO.println
-  IO.println last
-  let x := last.split (·.isAlpha) |> (·.mapM (·.toNat?)) |> (·.map (·.sum))|> (·.getD 0)
-  IO.println x
-  let map := Map.make map
-  map.strings.forM IO.println
-  let coms <- parseCommande.runIO last
-  IO.println map.start
-
-  let (map, x, y) := map.walk coms
-  IO.println coms
-  let d := coms.getLast?.get!.2
-  IO.println (x, y, d)
-  IO.println map.gone
-  map.strings.forM IO.println
-  let code := (x + 1) * 1000 + (y + 1) * 4 + d.code
-  IO.println code
+  let tail := (lines.dropWhile (· != "")).drop 1 
+  
+  let coms <- parseCommande.runIO tail[0]!
+  let side := tail[1]!.toNat!
+  for l in tail.drop 2 do
+    let glues <- parseGlues.runIO l
+    let map := Map.make map side glues 
+    let {map, dir, x, y} := map.walk coms
+    IO.println (x, y, dir)
+    map.strings.forM IO.println
+    let code := (x + 1) * 1000 + (y + 1) * 4 + dir.code
+    IO.println code
 
   
